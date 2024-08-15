fa00b11daa2c98274765eff984d5bc675d3bbbf9
razmikl <luluckyanr@yandex.ru>
2024-08-15 19:26:37 +0300

    update App.java, set user_name and print it on terminal

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,12 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Brain Games!");
+        Cli.yourName();
+
     }
 }
